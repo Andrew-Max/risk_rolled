6dbdec4dfc33ed1c8154e830d2898b2906551d9d
andrew <anm898989@gmail.com>
2017-02-05 17:26:02 -0500

    handles edge cases

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var aDice1, aDice2, aDice3, dDice1, dDice2;
 var attackDice, defenseDice;
 var attack_remaining, defense_remaining;
+var attack_win_counter = 0;
+var defense_win_counter = 0;
+var number_of_roles;
 
 $(document).ready(function () {
 
@@ -22,11 +25,31 @@ $(document).ready(function () {
   };
 
   function compareDice() {
-    var d1result = attackDice[0] > defenseDice[0];
-    var d2result = attackDice[1] > defenseDice[1];
+    var pair1result, pair2result;
 
-    d1result ? defense_remaining-- : attack_remaining--;
-    d2result ? defense_remaining-- : attack_remaining--;
+    if (attackDice[0] === defenseDice[0]) {
+      pair1result = null;
+    } else {
+      pair1result = attackDice[0] > defenseDice[0];
+    }
+
+    if(attackDice[1] && defenseDice[1]) {
+      if (attackDice[1] === defenseDice[1]) {
+        pair2result = null;
+      } else {
+        pair2result = attackDice[1] > defenseDice[1];
+      }
+    } else {
+      pair2result = null;
+    }
+
+    if(pair1result !== null) {
+      pair1result ? defense_remaining-- : attack_remaining--;
+    }
+
+    if(pair2result !== null && attack_remaining > 1 && defense_remaining > 0) {
+      pair2result ? defense_remaining-- : attack_remaining--;
+    }
   };
 
   function setDebugInfo() {
@@ -39,39 +62,37 @@ $(document).ready(function () {
     $('.defense-dice .val').text(defenseDice);
     $('.defense-remaining .val').text(defense_remaining);
     $('.attack-remaining .val').text(attack_remaining);
+    $('.num-roles .val').text(number_of_roles);
 
-    if(attack_remaining <= 0) {
+    if(attack_remaining <= 1) {
+      defense_win_counter++
       $('.result .val').text("Defense Keeps " + defense_remaining);
     }
 
     if(defense_remaining <= 0) {
+      attack_win_counter++
       $('.result .val').text("Attack Keeps " + attack_remaining);
     }
+    $('.attack-win .val').text(attack_win_counter);
+    $('.defense-win .val').text(defense_win_counter);
   };
 
   function rollSingleDice() {
     return Math.floor( (Math.random() * 6 ) + 1);
   };
 
-  function fakeRollAllDice() {
-    aDice1 = rollSingleDice();
-    aDice2 = rollSingleDice();
-    aDice3 = rollSingleDice();
-    bDice1 = rollSingleDice();
-    bDice2 = rollSingleDice();
-  }
-
   function rollAllDice(attack_remaining, defense_remaining) {
     if (attack_remaining >= 4) {
       aDice1 = rollSingleDice();
       aDice2 = rollSingleDice();
       aDice3 = rollSingleDice();
-    } else if (attack_remaining = 3) {
+    } else if (attack_remaining === 3) {
       aDice1 = rollSingleDice();
       aDice2 = rollSingleDice();
       aDice3 = undefined;
 
     } else {
+      console.log('rolled single attack')
       aDice1 = rollSingleDice();
       aDice2 = undefined;
       aDice3 = undefined;
@@ -89,15 +110,16 @@ $(document).ready(function () {
 
 
   $('.role').on('click', function () {
+    number_of_roles = 0
     resetDice();
     var attack_initial = parseInt($('.offense input').val());
     var defense_initial = parseInt($('.defense input').val());
     attack_remaining = attack_initial;
     defense_remaining = defense_initial
 
-    // while(attack_remaining > 1 && defense_remaining > 0) {
-    // }
-    while(attack_remaining > 0 && defense_remaining > 0) {
+
+    while(attack_remaining > 1 && defense_remaining > 0) {
+      number_of_roles++;
       rollAllDice(attack_remaining, defense_remaining);
       attackDice = [aDice1, aDice2, aDice3];
       defenseDice = [dDice1, dDice2];
@@ -105,6 +127,7 @@ $(document).ready(function () {
       compareDice();
       setDebugInfo();
     }
+    setDebugInfo();
   });
 
 
